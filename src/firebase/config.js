bffb1a9e40057722f182702b99d8972def2c5c53
zzacong <ongzz100@gmail.com>
2021-06-11 16:58:39 +1000

    chore: firebase config use env fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,11 +5,11 @@ import 'firebase/firestore'
 // FIREBASE WEB APP CONFIG
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  authDomain: 'firegram-15a80.firebaseapp.com',
+  databaseURL: 'https://firegram-15a80.firebaseio.com',
+  projectId: 'firegram-15a80',
+  storageBucket: 'firegram-15a80.appspot.com',
+  messagingSenderId: '774972276983',
   appId: process.env.REACT_APP_APP_ID,
 }
 
